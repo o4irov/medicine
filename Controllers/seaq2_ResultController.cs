7a13970e740fa38903eafc4a9712871aea209335
Andrew Ochirov <o41rov.a38@gmail.com>
2023-01-01 21:23:19 +0800

    контроллеры

---
+++ Controllers/seaq2_ResultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using лр5.Models;
+
+namespace лр5.Controllers
+{
+    public class seaq2_ResultController : Controller
+    {
+        private MedicineEntities db = new MedicineEntities();
+
+        // GET: seaq2_Result
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult seaq2Query(string name)
+        {
+            var query = "exec seaq2 '" + name + "'";
+            Console.WriteLine(query);
+            var data = db.Database.SqlQuery<seaq2_Result>(query);
+            return View(data.ToList());
+        }
+    }
+}
---
+++ Controllers/seaq_ResultController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using лр5.Models;
+
+namespace лр5.Controllers
+{
+    public class seaq_ResultController : Controller
+    {
+        private MedicineEntities db = new MedicineEntities();
+
+        // GET: seaq_Result
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult seaqQuery(int i, string val)
+        {
+            var query = "exec seaq '" + i.ToString() + "', '" + val + "'";
+            Console.WriteLine(query);
+            var data = db.Database.SqlQuery<seaq_Result>(query);
+            return View(data.ToList());
+        }
+    }
+}
